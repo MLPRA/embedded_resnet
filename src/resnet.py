bc94471ba08452d00f2fcc7526925e3ed52f15ec
Friedrich Schöne <schoene@livionda.com>
2018-01-26 12:15:08 +0100

    changed resnet feature vector output

--- src/resnet.py
+++ src/resnet.py
@@ -3,10 +3,11 @@ from chainer import Variable
 from chainer.dataset import concat_examples
 from chainer.links.model.vision.resnet import prepare
 
+
 class ResNet50Layers(chainer.links.ResNet50Layers):
 
     def _layer_out(self, images, layer):
-        layers = ['conv1', 'bn1', 'res2', 'res3', 'res4', 'res5', 'fc6', 'prob']
+        layers = ['conv1', 'bn1', 'res2', 'res3', 'res4', 'res5', 'pool5', 'fc6', 'prob']
         if layer not in layers:
             raise ValueError('Layer {} does not exist.'.format(layer))
 
@@ -19,7 +20,7 @@ class ResNet50Layers(chainer.links.ResNet50Layers):
         return y
 
     def feature_vector(self, images):
-        return self._layer_out(images, 'res5')
+        return self._layer_out(images, 'pool5')
 
     def predict(self, images):
         return self._layer_out(images, 'prob')
